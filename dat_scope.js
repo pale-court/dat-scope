0cc45e30fba4a397b36d5fd8c1677ad2092a68d4
Lars Viklund <zao@zao.se>
2024-04-20 19:25:22 +0200

    feat: show if chosen build is missing

--- dat_scope.js
+++ dat_scope.js
@@ -88,38 +88,52 @@ async function SyncBuilds() {
     console.log(doc_url.searchParams);
     window.history.pushState({ path: doc_url.toString() }, "", doc_url);
 
-    const resps = await Promise.all([DownloadBuild(from_bid), DownloadBuild(to_bid)]);
-    build_from = resps[0];
-    build_to = resps[1];
-    console.log(resps);
+    try {
+        const resps = await Promise.all([DownloadBuild(from_bid), DownloadBuild(to_bid)]);
+        build_from = resps[0];
+        build_to = resps[1];
+        console.log(resps);
+    }
+    catch (e) {
+        build_from = null;
+        build_to = null;
+    }
 
     var diff_host = document.querySelector("#diff-host");
 
     // Remove old diff elements
-    diff_host.querySelectorAll("dat-diff").forEach(elem => elem.remove());
+    while (diff_host.firstChild) {
+        diff_host.removeChild(diff_host.lastChild);
+    }
 
-    // Add new ones
-    const filename_keys = Object.keys(build_from.files).concat(Object.keys(build_to.files)).sort();
-    const filenames = new Set(filename_keys);
-    filenames.forEach(file => {
-        const from_file = build_from.files[file];
-        const to_file = build_to.files[file];
-        if (from_file && to_file) {
-            var allSame = true;
-            for (const field of Object.keys(field_labels)) {
-                if (from_file.stats[field] != to_file.stats[field]) {
-                    allSame = false;
+    if (!build_from || !build_to) {
+        // Communicate that one of the builds could not be obtained.
+        diff_host.innerHTML = "Could not obtain one of the builds.<br>They may either be old enough to not have DAT64 files or new enough that the system has not ingested them yet.";
+    }
+    else {
+        // Add new ones
+        const filename_keys = Object.keys(build_from.files).concat(Object.keys(build_to.files)).sort();
+        const filenames = new Set(filename_keys);
+        filenames.forEach(file => {
+            const from_file = build_from.files[file];
+            const to_file = build_to.files[file];
+            if (from_file && to_file) {
+                var allSame = true;
+                for (const field of Object.keys(field_labels)) {
+                    if (from_file.stats[field] != to_file.stats[field]) {
+                        allSame = false;
+                    }
+                }
+                if (allSame) {
+                    // Skip this file as it's of the same shape.
+                    return;
                 }
             }
-            if (allSame) {
-                // Skip this file as it's of the same shape.
-                return;
-            }
-        }
-        const elem = document.createElement("dat-diff");
-        elem.setAttribute("file", file);
-        diff_host.appendChild(elem);
-    });
+            const elem = document.createElement("dat-diff");
+            elem.setAttribute("file", file);
+            diff_host.appendChild(elem);
+        });
+    }
 }
 
 fetch(new URL("./global.json", dat_meta_root)).then(resp => {
