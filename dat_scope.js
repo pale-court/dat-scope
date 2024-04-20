b07bac3ad3ef25dc72bbffa336939b6b4a734960
Lars Viklund <zao@zao.se>
2024-04-20 17:48:43 +0200

    feat: initial commit, baseline diff

---
+++ dat_scope.js
@@ -0,0 +1,161 @@
+const dat_meta_root = new URL("https://raw.githubusercontent.com/pale-court/dat-meta/main/");
+
+const sel_from = document.querySelector("#sel-from");
+const sel_to = document.querySelector("#sel-to");
+
+const doc_url = new URL(document.URL);
+const doc_params = doc_url.searchParams;
+
+const desired_from = doc_params.get("from");
+const desired_to = doc_params.get("to");
+
+const build_data = {};
+var from_bid, to_bid;
+
+const field_labels = {
+    fixed_size: "Fixed section size",
+    row_count: "Fixed row count",
+    row_width: "Fixed row width",
+    var_offset: "Variable section offset",
+    var_size: "Variable section size",
+};
+
+class DatDiff extends HTMLElement {
+    static observedAttributes = ["file"];
+
+    constructor() {
+        super();
+        this.attr = {};
+    }
+
+    connectedCallback() {
+        const from_file = build_data[from_bid].files[this.attr.file];
+        const to_file = build_data[to_bid].files[this.attr.file];
+        if (from_file && !to_file) {
+            this.innerHTML = `${from_file.pretty_name} removed`;
+        } else if (!from_file && to_file) {
+            this.innerHTML = `${to_file.pretty_name} added`;
+        } else {
+            this.innerHTML = `${to_file.pretty_name} changed<br>`;
+            for (const [field, label] of Object.entries(field_labels)) {
+                console.log(field);
+                console.log(from_file);
+                if (from_file.stats[field] != to_file.stats[field]) {
+                    const d = document.createElement('div');
+                    d.textContent = `${label}: ${from_file.stats[field]} became ${to_file.stats[field]}`;
+                    this.appendChild(d);
+                }
+            }
+        }
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.attr[name] = newValue;
+        console.log(
+            `Attribute ${name} has changed from ${oldValue} to ${newValue}.`,
+        );
+    }
+}
+
+customElements.define('dat-diff', DatDiff);
+
+async function DownloadBuild(bid) {
+    if (build_data[bid]) {
+        return Promise.resolve(build_data[bid]);
+    }
+    const resp = await fetch(new URL(`builds/build-${bid}.json`, dat_meta_root));
+    const js = await resp.json();
+    var newFiles = {};
+    Object.keys(js.files).forEach(f => {
+        const fields = js.files[f];
+        const dat_re = /^data\/(\w+)\.dat(?:64)?$/i;
+        newFiles[f.toLowerCase()] = {
+            stats: fields,
+            pretty_name: dat_re.exec(f)[1]
+        };
+    });
+    js.files = newFiles;
+    build_data[bid] = js;
+    console.log(js);
+    return js;
+}
+
+async function SyncBuilds() {
+    from_bid = sel_from.item(sel_from.selectedIndex).value;
+    to_bid = sel_to.item(sel_to.selectedIndex).value;
+    doc_url.searchParams.set("from", from_bid);
+    doc_url.searchParams.set("to", to_bid);
+    console.log(doc_url.searchParams);
+    window.history.pushState({ path: doc_url.toString() }, "", doc_url);
+
+    const resps = await Promise.all([DownloadBuild(from_bid), DownloadBuild(to_bid)]);
+    build_from = resps[0];
+    build_to = resps[1];
+    console.log(resps);
+
+    var diff_host = document.querySelector("#diff-host");
+
+    // Remove old diff elements
+    diff_host.querySelectorAll("dat-diff").forEach(elem => elem.remove());
+
+    // Add new ones
+    const filename_keys = Object.keys(build_from.files).concat(Object.keys(build_to.files)).sort();
+    const filenames = new Set(filename_keys);
+    filenames.forEach(file => {
+        const from_file = build_from.files[file];
+        const to_file = build_to.files[file];
+        if (from_file && to_file) {
+            var allSame = true;
+            for (const field of Object.keys(field_labels)) {
+                if (from_file.stats[field] != to_file.stats[field]) {
+                    allSame = false;
+                }
+            }
+            if (allSame) {
+                // Skip this file as it's of the same shape.
+                return;
+            }
+        }
+        const elem = document.createElement("dat-diff");
+        elem.setAttribute("file", file);
+        diff_host.appendChild(elem);
+    });
+}
+
+fetch(new URL("./global.json", dat_meta_root)).then(resp => {
+    resp.json().then(js => {
+        console.log(js);
+        builds = js.builds;
+        const build_ids = Object.keys(builds).sort((a, b) => parseInt(b) - parseInt(a));
+        build_ids.forEach(bid => {
+            const build = builds[bid];
+            const opt = document.createElement("option");
+            opt.value = bid;
+            opt.text = `${build.game_version} (build ${bid})`;
+            sel_from.add(opt.cloneNode(true));
+            sel_to.add(opt);
+        });
+        if (desired_from && builds[desired_from]) {
+            sel_from.value = desired_from;
+        }
+        else {
+            sel_from.value = sel_from.options[1].value;
+        }
+
+        if (desired_to && builds[desired_to]) {
+            sel_to.value = desired_to;
+        } else {
+            sel_to.value = sel_to.options[0].value;
+        }
+
+        SyncBuilds();
+    });
+});
+
+const sel_change_handler = ev => {
+    console.log(ev);
+    SyncBuilds();
+};
+
+sel_from.onchange = sel_change_handler;
+sel_to.onchange = sel_change_handler;
